921843768ee39005ad1094b8670ddf05ae039e70
benlaender <benito@bodtlander.com>
2024-04-16 08:26:56 +0200

    Code Quality Change 2.0

--- backend/src/test/java/ch/zhaw/iwi/devops/CallElevator/CallElevator_Test.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/CallElevator/CallElevator_Test.java
@@ -5,52 +5,52 @@ import org.junit.jupiter.api.Test;
 import ch.zhaw.iwi.devops.demo.Elevator;
 
 
-public class CallElevator_Test
+class CallElevator_Test
 {
     @Test
-    public void Test1()
+    private void Test1()
     {
         Elevator Button = new Elevator();
         Assertions.assertEquals("Door opens", Button.call(0, 0));
     }
 
     @Test
-    public void Test2()
+    private void Test2()
     {
         Elevator Button = new Elevator();
         Assertions.assertEquals("Up", Button.call(1, 2));
     }
 
     @Test
-    public void Test3()
+    private void Test3()
     {
         Elevator Button = new Elevator();
         Assertions.assertEquals("Down", Button.call(3, -1));
     }
 
     @Test
-    public void Test4()
+    private void Test4()
     {
         Elevator Button = new Elevator();
         Assertions.assertEquals("Down", Button.call(500, -1));
     }
 
     @Test
-    public void Test5()
+    private void Test5()
     {
         Elevator Button = new Elevator();
         Assertions.assertEquals("Down", Button.call(-100, -1));
     }
 
     @Test
-    public void Test6()
+    private void Test6()
     {
         Elevator Button = new Elevator();
         Assertions.assertNotEquals("Down", Button.call(300,1));
     }
 
     @Test
-    public void Test7()
+    private void Test7()
     {
         Elevator Button = new Elevator();
         Assertions.assertEquals("Wrong building", Button.call(-2, 500));
